426aeadd38e5b651e20c8b0029aa1b8e7e7c7fd7
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-04-26 18:51:32 -0700

    heroku deployment

---
+++ pseudocode.js
@@ -0,0 +1,65 @@
+//////////////////////////////
+// On Da Rox Manager App
+//////////////////////////////
+
+/*
+
+// Pseudo code: App outline
+
+1. Set up all my configurations
+    X- NPM init -y
+    X- download all dependencies and sub dependencies
+        X- set up .env and gitignore
+    X- Set up all my dep and middleware
+    X- Create github repo and commit
+
+2. Create all my Routes (For Full Crud Functionality)
+    X- Create my Models, Views, Public (/static) folders
+    X- Create my model database with 5/11 drinks (/2 right now)
+    X- Create all my Routes and EJS files (INDUCES)
+        X- Home 
+            X- Create EJS file
+        X- Index 
+            X- Create my partials file
+            X- Index.ejs
+        X- New (create)
+            X- New.ejs
+        X- Update
+        X- Create
+        X- Edit
+            X- Edit.ejs
+        X- Show Route
+            X- Show.ejs
+        X- Show all the correct data in all Routes
+               
+3. Link to DB
+    X- Create Schema with Mongoose
+    X- Connect to Mongo DB 
+        Collection: ondaroxdb
+    X- Test Schema with json files
+    X- Test Mongo DB connection
+
+3.1. Style pages
+    X- Create Home page
+    - Style all pages (like wireframe)
+    - Try flexbox for index 3 per roll
+    X- Create 11 drinks (classics)
+
+3.2. Deploy on Heroku
+    - Deploy
+
+3.3. Develop a nice readme.md (inspired by OnDaRox readme)
+    - Do readme.md
+
+4. Extra (optionals)
+    X- Create Partials
+    X- Create public/static folder for CSS framework
+    - Create a navegator
+    - Authenthication login
+    - API that can feed/seed On Da Rox user app
+
+5. Bug
+
+Ps. Commit consistantly
+
+*/
--- server.js
+++ server.js
@@ -2,66 +2,7 @@
 // On Da Rox Manager App
 //////////////////////////////
 
-/*
 
-// Pseudo code: App outline
-
-1. Set up all my configurations
-    X- NPM init -y
-    X- download all dependencies and sub dependencies
-        X- set up .env and gitignore
-    X- Set up all my dep and middleware
-    X- Create github repo and commit
-
-2. Create all my Routes (For Full Crud Functionality)
-    X- Create my Models, Views, Public (/static) folders
-    - Create my model database with 5/11 drinks (/2 right now)
-    X- Create all my Routes and EJS files (INDUCES)
-        X- Home 
-            X- Create EJS file
-        X- Index 
-            X- Create my partials file
-            X- Index.ejs
-        X- New (create)
-            X- New.ejs
-        X- Update
-        X- Create
-        X- Edit
-            X- Edit.ejs
-        X- Show Route
-            X- Show.ejs
-        X- Show all the correct data in all Routes
-               
-3. Link to DB
-    X- Create Schema with Mongoose
-    X- Connect to Mongo DB 
-        Collection: ondaroxdb
-    X- Test Schema with json files
-    X- Test Mongo DB connection
-
-3.1. Style pages
-    X- Create Home page
-    - Style all pages (like wireframe)
-    - Try flexbox for index 3 per roll
-    X- Create 11 drinks (classics)
-
-3.2. Deploy on Heroku
-    - Deploy
-
-3.3. Develop a nice readme.md (inspired by OnDaRox readme)
-    - Do readme.md
-
-4. Extra (optionals)
-    X- Create Partials
-    X- Create public/static folder for CSS framework
-    - Authenthication login
-    - API that can feed/seed On Da Rox user app
-
-5. Bug
-
-Ps. Commit consistantly
-
-*/
 
 //////////////////////////////
 // Dependencies
@@ -69,13 +10,14 @@ Ps. Commit consistantly
 
 // .env
 require("dotenv").config()
-// web framework
+// Web framework
 const express = require("express")
 // Object Document Manager (Work with DB)
 const mongoose = require("mongoose")
 // Override request methods (Post - Put / Post - Delete)
 const methodOverride = require("method-override")
-const morgan = require("morgan") // used for logging
+// Used for logging
+const morgan = require("morgan") 
 
 
 //////////////////////////////
@@ -188,8 +130,7 @@ app.get("/drink/seed", async (req, res) => {
             ingredient10: "",
             instructions: "In a blender add the berries, avocado, honey, and coconut water. Blend until smooth. If you need to stop and scrape the sides that will help too. It is delicious!",
             drinkHistory: "Made by Germano Kuerten in 2022"},
-
-            {idDrink: "002",
+        {idDrink: "002",
             alcoholic: "Yes",
             drinkName: "Mano's smoothie 2",
             drinkImage: "https://therecipecritic.com/wp-content/uploads/2016/03/blackberrylimesmoothie-650x975.jpg",
